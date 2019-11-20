beda64c2e778497617098241ba343d00144ed86f
Moonjinhan <answls7337@naver.com>
2019-11-20 23:02:48 +0900

    up

---
+++ 1차분석/NLP_TEST.py
@@ -0,0 +1,29 @@
+from konlpy.tag import Komoran
+from pprint import pprint
+import numpy
+import scipy
+
+komorean=Komoran() 
+text_file=open("C:/Users/user/Documents/Moons/1차분석/test2.txt",encoding='utf-8')
+
+text = text_file.read()
+pprint(komorean.pos(text))
+
+
+# print(text)
+
+# while True:
+#     text=text_file.readline()
+#     if not text: break
+#     print(text)
+#     # Text_list=[]
+#     # for page in 
+
+    
+
+
+
+#print(komorean.pos(u'text_file'))
+
+#참고: 다음기사 크롤링 https://leegijun.tistory.com/5
+#참고: TF-IDF 라이브러리 - Scikit-Learn : https://blog.naver.com/shrntg/220655244241
--- 형태소분석/NLP_TEST.py
+++
@@ -1,6 +0,0 @@
-from konlpy.tag import Komoran
-import numpy
-
-komorean=Komoran() 
-
-print(komorean.pos(u'우왕 코모란도 오픈소시가 되었어요'))
