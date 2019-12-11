0cf540afa66c46d168d29b39711b097634016d04
Moonjinhan <answls7337@naver.com>
2019-12-11 21:51:41 +0900

    20191211

--- 분석/knusl test.py
+++ 분석/knusl test.py
@@ -25,27 +25,9 @@ class KnuSL():
 if __name__ == "__main__":
 	
 	ksl = KnuSL
-	
-	print("\nKNU 한국어 감성사전입니다~ :)")
-	print("사전에 단어가 없는 경우 결과가 None으로 나타납니다!!!")
-	print("종료하시려면 #을 입력해주세요!!!")
-	print("-2:매우 부정, -1:부정, 0:중립 or Unkwon, 1:긍정, 2:매우 긍정")
-	print("\n")	
 
 	for i in match_char[0:]:
 		i = i.strip(" ")
 		point = ksl.data_list(i)
 		for total in point[0:]:
 			total += i
-
-	while(True):
-		wordname = input("word : ")
-		wordname = wordname.strip(" ")		
-		if wordname != "#":
-			print(ksl.data_list(wordname))
-			print("\n")	
-				
-
-		elif wordname == "#":
-			print("\n이용해주셔서 감사합니다~ :)")
-			break
--- 분석/test_re.py
+++ 분석/test_re.py
@@ -33,3 +33,36 @@ for i in result:
         m = re.match(i,sents,re.MULTILINE) 
         print(m)
         
+##########################################################################################
+
+class KnuSL()
+    def data_list(wordname):	
+            with open('C:/Users/student/Documents/GitHub/Moons/분석/data/SentiWord_info.json', encoding='utf-8-sig', mode='r') as f:
+                data = json.load(f)
+            result = ['None']	
+            for i in range(0, len(data)):
+                if data[i]['word'] == wordname:
+                    result.pop()
+                    result.pop()
+                    result.append(data[i]['polarity'])	
+            
+        
+            s_word = result[1]
+            return s_word
+
+if __name__ == "__main__":
+	
+	ksl = KnuSL
+
+    for word in match_char[0:]:
+    while(True):
+		wordname = input("word : ")
+		wordname = wordname.strip(" ")		
+		if wordname != "#":
+			print(ksl.data_list(wordname))
+			print("\n")	
+				
+
+		elif wordname == "#":
+			print("\n이용해주셔서 감사합니다~ :)")
+			break
